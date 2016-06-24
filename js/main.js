7db46c559fa1632d9cdde092fe46bc64b0dafc94
Maggie <Maggie@ZhedeMacBook-Air.local>
2016-06-23 22:55:50 -0700

    ++

--- js/main.js
+++ js/main.js
@@ -124,5 +124,26 @@ $(window).load(function() {
 	
 	$('input, textarea').placeholder();
 
+
+/* ----------------------------- 
+Scroll into viewPort Animation
+----------------------------- */
+$(document).ready(function() {	
+	'use strict';
+	$('.animated').appear(function() {
+		var element = $(this),
+			animation = element.data('animation'),
+			animationDelay = element.data('animation-delay');
+			if ( animationDelay ) {
+
+				setTimeout(function(){
+					element.addClass( animation + " visible");
+				}, animationDelay);
+
+			} else {
+				element.addClass( animation + " visible");
+			}
+	});
+});
 	
   
